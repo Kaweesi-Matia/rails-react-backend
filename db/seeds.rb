7325c52b4d91ba12b289fac97d2dd332ce7c975e
Kaweesi Matia <matiakaweesi@gmail.com>
2023-03-04 02:49:56 +0300

    Populate seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create!(greeting: "welcome to Africa")
+Greeting.create!(greeting: "This is fantastic")
+Greeting.create!(greeting: "Happy coding")
+Greeting.create!(greeting: "it is so cold")
+Greeting.create!(greeting: "it is time for relaxing")
